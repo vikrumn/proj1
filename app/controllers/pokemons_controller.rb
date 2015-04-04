f0015f2d45d8dba7c7b98e78515aa6a4b4e1cc90
vikrumn <vikrumn@Vikrums-MacBook.local>
2015-04-03 18:49:38 -0700

    Saving before resetting db

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,11 @@
+class PokemonsController < ApplicationController
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer_id = current_trainer.id
+		@pokemon.save
+		redirect_to root_url
+	end
+	def index
+		@pokemons = Pokemon.all
+	end
+end
